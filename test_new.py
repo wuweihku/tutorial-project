e195d0bb849aca95bd9fd0610898d5ebdce8f8de
wuwei <wuweihku@163.com>
2015-12-11 17:27:01 +0800

    2

--- test_new.py
+++ test_new.py
@@ -3,21 +3,29 @@ import urllib
 import urllib2
 
 class TestGet(unittest.TestCase):
-    def test_register(self):
+
+    url = 'http://passportapi.15166.com/user/signup'
+
+    def setUp(self):
+        pass
+    def tearDown(self):
+        pass
+
+    def test_wuwei(self):
         
         params =urllib.urlencode({'appid': '4e7dHsOyi5QjgpiBiemIreX74f7VgUCg7L', 'username': 'wuweihku', 'password': '123456', 'repassword': '123456'})     
         
-        url = 'http://passportapi.15166.com/user/signup'
-
-        req = urllib2.urlopen(url, params)
+        req = urllib2.urlopen(TestGet.url, params)
 
         content = eval(req.read())
 
-        self.assertEqual(content["res"], 10008)
+        self.assertEqual(content["res"], 10001)
 #######################################################################
+    def test_qahechang(self):
+
         params2 =urllib.urlencode({'appid': '4e7dHsOyi5QjgpiBiemIreX74f7VgUCg7L', 'username': 'qahechang', 'password': '123456', 'repassword': '123456'})
         
-        req = urllib2.urlopen(url, params2) 
+        req = urllib2.urlopen(TestGet.url, params2) 
 
         content2 = eval(req.read())
 
